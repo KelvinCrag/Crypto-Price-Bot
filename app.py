bf975ecb6893a06a5ae2a6afccfd0fdd2e12729c
Kelvin Crag <64832927+KelvinCrag@users.noreply.github.com>
2022-07-21 14:03:03 +0100

    Final_rc
    Attempt to finalize the text

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ client = Client()
 def startPrivate(msg : types.Message):
     bot.send_message(
         msg.chat.id,
-        '<b>Hi! Welcome to @iPapkornCrypto Bot. This Bot will help you to get info on the current market value of various coins!\n\nUsage : /p</b>',
+        '<b>Hiii!!\n\n💰Welcome to iPap Crypto Bot, this Bot will help you to get info on the current market value of various coins!</b>\n\nUsage : /p',
         parse_mode='html',
         reply_to_message_id=msg.id,
         disable_web_page_preview=True
