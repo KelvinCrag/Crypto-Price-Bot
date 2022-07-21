e8f5259879d60db33fdd569fc75118084955c582
Kelvin Crag <64832927+KelvinCrag@users.noreply.github.com>
2022-07-21 14:14:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ client = Client()
 def startPrivate(msg : types.Message):
     bot.send_message(
         msg.chat.id,
-        '<b>Hiii!!🤩\n\n💰Welcome to iPap Crypto Bot, this Bot will help you to get info on the current market value of various coins!</b>\n\nUsage : /p',
+        'Hiii!!🤩\n\n💰Welcome to iPap Crypto Bot! This Bot will help you get info on the current market value of various coins.\n\nUse the /p command to begin',
         parse_mode='html',
         reply_to_message_id=msg.id,
         disable_web_page_preview=True
