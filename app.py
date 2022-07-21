ace05a600d8ad64dff96bba15a9605e68bf812ca
Kelvin Crag <64832927+KelvinCrag@users.noreply.github.com>
2022-07-21 13:46:05 +0100

    rc_1
    Hopefully last fix

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ client = Client()
 def startPrivate(msg : types.Message):
     bot.send_message(
         msg.chat.id,
-        '<b>😄 Hi! Welcome to iPap Crypto Bot! This Bot is using the Binance API that enables people to get info about the market value of various coins\n\nUsage : /p <currency>\n\nJoin @iPapCrypto for your Crypto updates</b>',
+        '<b>Hi😄 This Bot will help you to get info about the market value of various coins\n\nUsage : /p <currency>\n\nJoin @iPapCrypto for your Crypto updates</b>',
         parse_mode='html',
         reply_to_message_id=msg.id,
         disable_web_page_preview=True
